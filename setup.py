9921b6a93c5e1fd95448598aa75dd035e6838976
Kevin Conway <kevinjacobconway@gmail.com>
2017-01-04 19:33:09 -0600

    Bump version for stale pid management

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('LICENSE') as f:
 
 setup(
     name='daemons',
-    version='1.2.2',
+    version='1.3.0',
     url='https://github.com/kevinconway/daemons',
     license=license,
     description='Well behaved unix daemons for every occasion.',
