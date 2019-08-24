0ef938a4c10df7e3cf71851095079ff9e119055f
Kevin Conway <kevinjacobconway@gmail.com>
2019-08-24 17:25:52 -0500

    Bump version to release README to PyPi

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as f:
+with open("README.rst") as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open("LICENSE") as f:
     license = f.read()
 
 setup(
-    name='daemons',
-    version='1.3.0',
-    url='https://github.com/kevinconway/daemons',
+    name="daemons",
+    version="1.3.1",
+    url="https://github.com/kevinconway/daemons",
     license=license,
-    description='Well behaved unix daemons for every occasion.',
-    author='Kevin Conway',
-    author_email='kevinjacobconway@gmail.com',
+    description="Well behaved unix daemons for every occasion.",
+    author="Kevin Conway",
+    author_email="kevinjacobconway@gmail.com",
     long_description=readme,
     classifiers=[],
-    packages=find_packages(exclude=['tests', 'build', 'dist', 'docs']),
+    packages=find_packages(exclude=["tests", "build", "dist", "docs"]),
     requires=[],
 )
