0eb5e0008d54f48f7643ca1dfbefc861c082aa63
Kevin Conway <kevinjacobconway@gmail.com>
2019-08-24 18:59:10 -0500

    Remove multi-line license entry
    This field must be a single line to produce a valid PKG-INFO file.

--- setup.py
+++ setup.py
@@ -4,19 +4,16 @@ from setuptools import setup, find_packages
 with open("README.rst") as f:
     readme = f.read()
 
-with open("LICENSE") as f:
-    license = f.read()
-
 setup(
     name="daemons",
     version="1.3.1",
     url="https://github.com/kevinconway/daemons",
-    license=license,
+    license="'Apache License 2.0",
     description="Well behaved unix daemons for every occasion.",
     author="Kevin Conway",
     author_email="kevinjacobconway@gmail.com",
     long_description=readme,
     classifiers=[],
     packages=find_packages(exclude=["tests", "build", "dist", "docs"]),
-    requires=[],
+    install_requires=[],
 )
